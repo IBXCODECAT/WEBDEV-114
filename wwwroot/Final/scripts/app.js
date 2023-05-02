4abe2d257e2309e581a95d29048eed0677fb9609
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-05-02 09:43:48 -0500

    Javascript, Rework Navigation, Contact PHP

--- wwwroot/Final/scripts/app.js
+++ wwwroot/Final/scripts/app.js
@@ -1,1 +1,11 @@
 console.log("Scripts would go here!")
+
+function AddBlueBot()
+{
+    const response = confirm("You are about to be redirected outside Blue Bot's website.");
+
+    if(response)
+    {
+        window.open('https://canary.discord.com/api/oauth2/authorize?client_id=988833332445978624&permissions=536870944&scope=bot%20applications.commands', '_blank');
+    }
+}
